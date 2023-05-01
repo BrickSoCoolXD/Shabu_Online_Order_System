128a8fe78c6adfaf03cb85cd416db4415f53f7c0
Nimit Tanbooutor <nimittanbooutor@gmail.com>
2023-05-01 22:43:14 +0700

    Updated Catch & Try with Handling Exception feature
    Using new feature as exception (catch) and show the error message

--- Main.java
+++ Main.java
@@ -2,38 +2,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Order order = new Order();
+        try {
+            Scanner sc = new Scanner(System.in);
+            Order order = new Order();
 
-        System.out.println("*************** Welcome To Shabu Order Online ***************");
-        System.out.println("=============================================================");
-        Menu menu = new Menu();
-        menu.showMenu();
-        System.out.println("=============================================================");
-        boolean orderMore = true;
-        while (orderMore) {
-            System.out.println("What Do You Want to Order Today");
-            int choice = sc.nextInt();
+            System.out.println("*************** Welcome To Shabu Order Online ***************");
+            System.out.println("=============================================================");
+            Menu menu = new Menu();
+            menu.showMenu();
+            System.out.println("=============================================================");
+            boolean orderMore = true;
+            while (orderMore) {
+                System.out.println("What Do You Want to Order Today");
+                int choice = sc.nextInt();
 
-            if (choice == 28) {
-                orderMore = false;
-                continue;
+                if (choice == 28) {
+                    orderMore = false;
+                    continue;
+                }
+
+                MenuItem menuItem = menu.getMenuItems().get(choice - 1);
+                System.out.println("You have Selected " + menuItem.getName());
+
+                System.out.println("Enter the desired Quantity : ");
+                int quantity = sc.nextInt();
+
+                order.addToOrder(menuItem, quantity);
+
+                System.out.println("Do you wish to order anything else (Y/N) : ");
+                String choiceString = sc.next();
+                orderMore = choiceString.equalsIgnoreCase("Y");
             }
 
-            MenuItem menuItem = menu.getMenuItems().get(choice - 1);
-            System.out.println("You have Selected " + menuItem.getName());
-
-            System.out.println("Enter the desired Quantity : ");
-            int quantity = sc.nextInt();
-
-            order.addToOrder(menuItem, quantity);
-
-            System.out.println("Do you wish to order anything else (Y/N) : ");
-            String choiceString = sc.next();
-            orderMore = choiceString.equalsIgnoreCase("Y");
+            System.out.println("******************** Thank you for ordering ********************");
+            order.printReceipt();
+        } catch (Exception e) {
+            System.out.println("An error occurred while processing your order: " + e.getMessage());
         }
-
-        System.out.println("******************** Thank you for ordering ********************");
-        order.printReceipt();
     }
 }
